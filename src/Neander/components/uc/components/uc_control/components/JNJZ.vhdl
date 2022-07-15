7a06d43d894a37362d5fa2cc98e94114e9fd3b2f
pfzoz <pedrozoz.sizaan@gmail.com>
2022-07-15 13:59:24 -0300

    Initialized Jump

--- src/Neander/components/uc/components/uc_control/components/JNJZ.vhdl
+++ src/Neander/components/uc/components/uc_control/components/JNJZ.vhdl
@@ -12,14 +12,18 @@ end entity;
 
 architecture comute of JNJZ is
 
+    signal matters : std_logic;
+
 begin 
+
+    matters <= intFlags(1) when dec2uc = 
 
     barr_stuff(10) <= not(counter(2));
     barr_stuff(9)  <= (not(counter(2)) and not(counter(1))) or (not(counter(2)) and counter(1) and not(counter(0)));
     barr_stuff(8)  <= '0';
     barr_stuff(7)  <= '0';
     barr_stuff(6)  <= '0';
-    barr_stuff(5)  <= not(counter(2)) and (counter(0));
+    barr_stuff(5)  <= not(counter(2)) and (counter(0)) when sinalqueimporta = ""
     barr_stuff(4)  <= not(counter(2)) and (counter(1) and not(counter(0)));
     barr_stuff(3)  <= (not(counter(2)) and not(counter(1)) and counter(0));
     barr_stuff(2)  <= not(counter(2)) and not(counter(1)) and not(counter(0));
